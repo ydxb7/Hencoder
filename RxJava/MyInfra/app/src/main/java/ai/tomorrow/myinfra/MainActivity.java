adb1c96f7ebcab60e65275e9cf36104d7f3b4da2
Yini <ydxb7@mail.missouri.edu>
2020-01-05 23:51:25 -0800

    simple example

--- RxJava/MyInfra/app/src/main/java/ai/tomorrow/myinfra/MainActivity.java
+++ RxJava/MyInfra/app/src/main/java/ai/tomorrow/myinfra/MainActivity.java
@@ -1,16 +1,44 @@
 package ai.tomorrow.myinfra;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+        setup();
     }
 
+    private void setup() {
+        Single.just("1")
+                .subscribe(new SingleObserver<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
 
+                    }
+
+                    @Override
+                    public void onSuccess(String s) {
+                        textView.setText(s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                });
+    }
 }
