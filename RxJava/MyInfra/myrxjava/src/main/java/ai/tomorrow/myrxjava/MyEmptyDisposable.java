eb4ef172e2f38753f18ad65940e098397294a58a
Yini <ydxb7@mail.missouri.edu>
2020-01-07 23:47:22 -0800

    remove empty lines

--- RxJava/MyInfra/myrxjava/src/main/java/ai/tomorrow/myrxjava/MyEmptyDisposable.java
+++ RxJava/MyInfra/myrxjava/src/main/java/ai/tomorrow/myrxjava/MyEmptyDisposable.java
@@ -3,16 +3,11 @@ package ai.tomorrow.myrxjava;
 public enum MyEmptyDisposable implements MyDisposable {
     INSTANCE, NEVER;
 
-
     @Override
-    public void dispose() {
-
-    }
+    public void dispose() { }
 
     @Override
     public boolean isDisposed() {
         return this == INSTANCE;
     }
-
-
 }
